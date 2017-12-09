79fd07c8c7f506afa83d53f57cb58ffda8151396
SapS <svsapegin@mail.ru>
2017-12-09 14:27:36 +0300

    Resolution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2 - doing, what they've wrote in manual
 
-## Write a short comment describing this function
+## Special object for storing matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinvert <- function(invert) m <<- invert
+  getinvert <- function() m
+  list(set = set, get = get,
+       setinvert = setinvert,
+       getinvert = getinvert)
+  
 }
 
-
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinvert()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinvert(m)
+  m
+  
 }
+
+## Example for test
+## MxEx <- matrix(c(2,4,3,1,5,7,6,6,6),nrow=3,ncol=3,byrow=TRUE)
+
